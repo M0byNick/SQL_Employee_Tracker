bc977be586181bee95aa088859ee4157858e4bea
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-07-29 18:52:49 -0400

    More seed arrays

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,2 +1,38 @@
 Use employeesDB;
 
+Insert into employee ()
+
+Insert into department (dept_name)
+Values ("Sales"),
+('HR'),
+('Engineering'),
+('Finance'),
+('Legal'),
+('IT'),
+('CSuite');
+
+Insert into role (title, salary, dept_ID)
+Values ('Account Manager', 100000, 1),
+('Sr. Account Manager', 150000, 1),
+('Sales Director', 200000, 1),
+('HR Coordinator', 70000, 2),
+('HR Generalist', 95000, 2),
+('HR Director', 125000, 2),
+('Software Engineer', 125000, 3),
+('Sr. Software Engineer', 175000, 3),
+('Engineering Lead', 220000, 3),
+('Engineering Director', 255000, 3),
+('Internal Accountant', 80000, 4),
+('Internal Auditing Lead', 115000, 4),
+('Attorney', 150000, 5),
+('Paralegal', 65000, 5),
+('Legal Analyst', 90000, 5),
+('IT Project Manager', 95000, 6),
+('IT Director', 125000, 6),
+('Chief Executive OFficer', 400000, 7),
+('Chief Operating Officer', 315000, 7),
+('Chief Financial Officer', 315000, 7),
+('Chief HR Manager', 315000, 7),
+('Chief Technology Officer', 315000, 7),
+('Chief Information Officer', 315000, 7),
+('General Counsel', 315000, 7);
